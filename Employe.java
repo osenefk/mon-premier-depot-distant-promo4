651b46dc6488fe2959c1e70d7ff690a29b9d89d0
Ousseynou SENE <seneousseynou89@gmail.com>
2018-03-14 20:44:01 +0100

    Ajout des fichiers Employe et Etudiant

---
+++ Employe.java
@@ -0,0 +1,25 @@
+public class Employe{
+
+//Proprietes
+	private String nomEmploye;
+	private String prenomEmploye;
+	
+	//Constructeurs
+	
+	public Employe(){
+		
+	}
+	
+	public Employe(String nom, String prenom){
+		
+		this.nomEmploye=nom;
+		
+		this.prenomEmploye=prenom;
+		
+	}
+	
+	
+	
+	
+	
+}
---
+++ Etudiant.java
@@ -0,0 +1,20 @@
+public class Etudiant{
+
+//Proprietes
+
+	private String nomEtudiant;
+	private String prenomEtudiant;
+	
+	//Constructeurs
+	
+	public Etudiant(){
+		
+	}
+	
+	public Etudiant(String nom, String prenom){
+		
+		this.nomEtudiant=nom;
+		
+		this.prenomEtudiant=prenom;
+		
+	}
